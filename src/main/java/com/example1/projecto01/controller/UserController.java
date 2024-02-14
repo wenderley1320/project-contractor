ae5d6fd2fba4631c40325ca7f686f7b7dea75069
Wenderley Ramon <wenderleymartin@gmail.com>
2024-02-13 22:36:40 -0300

    Añadí el controllador

--- src/main/java/com/example1/projecto01/controller/UserController.java
+++ src/main/java/com/example1/projecto01/controller/UserController.java
@@ -1,13 +1,12 @@
 package com.example1.projecto01.controller;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
+@Controller
 public class UserController {
-    @RequestMapping("/m/{nombre}/{apellido}/{edad}")
-    public String showLesson(@PathVariable("nombre") String wenderley, @PathVariable("apellido") String module, @PathVariable("edad") String lesson){
-    	return "Track: " + wenderley + ", Module: " + module + ", Lesson: " + lesson;
+    @GetMapping("/")
+    public String index(){
+        return "index.jsp";
     }
 }
